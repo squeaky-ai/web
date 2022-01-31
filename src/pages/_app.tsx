8d1eb36cb173fab092c3e158e5ab4fb7d683c4ed
Lewis Monteith <lewismonteith@gmail.com>
2022-01-31 20:23:47 +0000

    Add identify code

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,21 +4,34 @@ import type { AppProps } from 'next/app';
 import { ApolloProvider } from '@apollo/client';
 import { client } from 'lib/api/graphql';
 import { ToastProvider } from 'components/toast';
-import { UserProvider } from 'components/user';
 import { Page } from 'components/page';
 
 import '../styles/main.scss';
 
-const App: FC<AppProps> = ({ Component, pageProps }) => (
-  <ApolloProvider client={client}>
-    <ToastProvider>
-      <UserProvider>
+const App: FC<AppProps> = ({ Component, pageProps }) => {
+  React.useEffect(() => {
+    if (window.squeaky && pageProps.user) {
+      const { id, firstName, lastName, email, superuser, createdAt } = pageProps.user;
+
+      window.squeaky.identify(id, {
+        'First name': firstName,
+        'Last name': lastName,
+        'Email': email,
+        'Superuser': superuser,
+        'Created': createdAt,
+      });
+    }
+  }, []);
+
+  return (
+    <ApolloProvider client={client}>
+      <ToastProvider>
         <Page {...pageProps}>
           <Component {...pageProps} />
         </Page>
-      </UserProvider>
-    </ToastProvider>
-  </ApolloProvider>
-);
+      </ToastProvider>
+    </ApolloProvider>
+  );
+};
 
 export default App;
--- src/types/common.ts
+++ src/types/common.ts
@@ -1,8 +1,21 @@
 export type ValueOf<T> = T[keyof T];
 
+declare global {
+  interface Window {
+    squeaky: Squeaky;
+  }
+}
+
+type ExternalAttributes = Record<string, string | number>;
+
+interface Squeaky {
+  identify: (id: string, input: ExternalAttributes) => void;
+}
+
 export type Currency = { 
   name: string; 
   symbol: string 
 };
 
 export type SubMenu = 'use-cases' | 'product' | 'more';
+
