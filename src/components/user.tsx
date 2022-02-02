edffbdea21a1ff94e38e43639b01212a2656914f
Lewis Monteith <lewismonteith@gmail.com>
2022-02-02 09:38:55 +0000

    Add polling for squeaky

--- src/components/user.tsx
+++ src/components/user.tsx
@@ -3,6 +3,7 @@ import type { FC } from 'react';
 import getConfig from 'next/config';
 import Script from 'next/script';
 import type { User as UserType } from 'types/graphql';
+import { usePoll } from 'hooks/use-poll';
 
 const { publicRuntimeConfig } = getConfig();
 
@@ -12,34 +13,41 @@ interface Props {
   user: UserType;
 }
 
-export const User: FC<Props> = ({ children, user }) => (
-  <>
-    <Script 
-      id='squeaky-script'
-      dangerouslySetInnerHTML={{ __html: dev ? '' : `
-        (function(s,q,e,a,u,k,y){
-          s._sqSettings={site_id:'2918cf0f-42aa-499d-a4da-d362bd1011ed'};
-          u=q.getElementsByTagName('head')[0];
-          k=q.createElement('script');
-          k.src=e+s._sqSettings.site_id;
-          u.appendChild(k);
-        })(window,document,'https://cdn.squeaky.ai/g/0.4.0/script.js?');
-      `}}
-      onLoad={() => {
-        console.log(user, window.squeaky);
-        if (user && window.squeaky) {
-          const { id, firstName, lastName, email, superuser, createdAt } = user;
+export const User: FC<Props> = ({ children, user }) => {
+  const loaded = usePoll(() => {
+    return window.hasOwnProperty('squeaky');
+  });
 
-          window.squeaky.identify(id, {
-            'First name': firstName,
-            'Last name': lastName,
-            'Email': email,
-            'Superuser': superuser ? 'Yes' : 'No',
-            'Created': new Date(createdAt).toLocaleDateString(),
-          });
-        }
-      }}
-    />
-    {children}
-  </>
-);
+  React.useEffect(() => {
+    if (!user || !loaded) return;
+
+    const { id, firstName, lastName, email, superuser, createdAt } = user;
+
+    window.squeaky.identify(id, {
+      'First name': firstName,
+      'Last name': lastName,
+      'Email': email,
+      'Superuser': superuser ? 'Yes' : 'No',
+      'Created': new Date(createdAt).toLocaleDateString(),
+    });
+  }, [loaded]);
+
+  return (
+    <>
+      <Script 
+        id='squeaky-script'
+        strategy='beforeInteractive'
+        dangerouslySetInnerHTML={{ __html: dev ? '' : `
+          (function(s,q,e,a,u,k,y){
+            s._sqSettings={site_id:'2918cf0f-42aa-499d-a4da-d362bd1011ed'};
+            u=q.getElementsByTagName('head')[0];
+            k=q.createElement('script');
+            k.src=e+s._sqSettings.site_id;
+            u.appendChild(k);
+          })(window,document,'https://cdn.squeaky.ai/g/0.4.0/script.js?');
+        `}}
+      />
+      {children}
+    </>
+  );
+};
---
+++ src/hooks/use-poll.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+type PollCallback = () => boolean;
+
+type Resolve = (success: boolean) => void;
+
+function poll(callback: PollCallback, timeout = 5000, interval = 250) {
+  const endAt = new Date().valueOf() + timeout;
+
+  const checkCondition = function(resolve: Resolve): void {
+    const result = callback();
+
+    if (result) {
+      return resolve(true);
+    }
+    
+    if (new Date().valueOf() < endAt) {
+      return void setTimeout(checkCondition, interval, resolve);
+    }
+
+    return resolve(false);
+  }
+
+  return new Promise(checkCondition);
+}
+
+
+export const usePoll = (callback: PollCallback): boolean => {
+  const [loaded, setLoaded] = React.useState<boolean>(false);
+
+  React.useEffect(() => {
+    poll(callback).then(success => setLoaded(success));
+  }, []);
+
+  return loaded;
+};
