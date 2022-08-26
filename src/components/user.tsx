7c30c9b8257265c34645a4098167afa1c92cebdc
Lewis Monteith <lewismonteith@gmail.com>
2022-08-26 12:39:18 +0100

    Remove intercom

--- src/components/user.tsx
+++ src/components/user.tsx
@@ -43,16 +43,6 @@ export const User: FC<Props> = ({ children, user }) => {
 
   return (
     <>
-      <Script
-        id='intercom-settings'
-        strategy='afterInteractive'
-        dangerouslySetInnerHTML={{ __html: `
-          window.intercomSettings = {
-            api_base: 'https://api-iam.intercom.io',
-            app_id: 'opustw9o',
-          };
-        `}} 
-      />
       <Script 
         id='squeaky-script'
         strategy='afterInteractive'
@@ -65,14 +55,6 @@ export const User: FC<Props> = ({ children, user }) => {
             u.appendChild(k);
           })(window,document,'https://cdn.squeaky.ai/g/0.4.0/script.js?');
         `}}
-      />
-      <Script
-        id='intercom-script'
-        strategy='afterInteractive'
-        dangerouslySetInnerHTML={{ __html: dev ? '' : `
-          // We pre-filled your app ID in the widget URL: 'https://widget.intercom.io/widget/opustw9o'
-          (function () { var w = window; var ic = w.Intercom; if (typeof ic === "function") { ic('reattach_activator'); ic('update', w.intercomSettings); } else { var d = document; var i = function () { i.c(arguments); }; i.q = []; i.c = function (args) { i.q.push(args); }; w.Intercom = i; var l = function () { var s = d.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = 'https://widget.intercom.io/widget/opustw9o'; var x = d.getElementsByTagName('script')[0]; x.parentNode.insertBefore(s, x); }; if (document.readyState === 'complete') { l(); } else if (w.attachEvent) { w.attachEvent('onload', l); } else { w.addEventListener('load', l, false); } } })();
-        `}} 
       />
       {children}
     </>
