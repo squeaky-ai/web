69084a6a80267c4dc0477bb4f6f0754608c1548e
Lewis Monteith <lewismonteith@gmail.com>
2022-12-19 17:59:53 +0000

    Use squeaky 1.0 script

--- src/components/page.tsx
+++ src/components/page.tsx
@@ -78,7 +78,7 @@ export const Page: FC<Props> = ({ children, partner }) => {
             k=q.createElement('script');
             k.src=e+s._sqSettings.site_id;
             u.appendChild(k);
-          })(window,document,'https://cdn.squeaky.ai/g/0.4.0/script.js?');
+          })(window,document,'https://cdn.squeaky.ai/g/1.0.0/script.js?');
         `}}
       />
 
