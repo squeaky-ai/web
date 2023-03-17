a64f18c5df59969287f2990439325451efe83208
Lewis Monteith <lewismonteith@gmail.com>
2023-03-17 08:45:53 +0000

    Add more details to access loggign

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,9 +3,11 @@ import type { NextRequest } from 'next/server'
 export function middleware(request: NextRequest) {
   console.log(JSON.stringify({
     url: request.url,
+    method: request.method,
     geo: request.geo,
     ip: request.headers.get('x-forwarded-for'),
     useragent: request.headers.get('user-agent'),
+    timestamp: new Date().toISOString(),
   }));
 }
 
