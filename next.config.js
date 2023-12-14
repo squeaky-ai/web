0989ce087c405b041080c6b4fb409018d4325541
Lewis Monteith <lewismonteith@gmail.com>
2023-12-14 08:36:26 +0000

    Run the script locally on the website

--- next.config.js
+++ next.config.js
@@ -8,6 +8,8 @@ const APP_HOST_NAME = IS_DEV ? 'http://app.squeaky.test' : 'https://app.squeaky.
 
 const API_HOST_NAME = IS_DEV ? 'http://api.squeaky.test' : 'https://api.squeaky.ai';
 
+const SCRIPT_URL = IS_DEV ? 'http://localhost:8081/.build/script.js' : 'https://cdn.squeaky.ai/g/1.1.0/script.js';
+
 /**
  * @type {import('next').NextConfig}
  */
@@ -21,6 +23,7 @@ module.exports = {
     apiHost: API_HOST_NAME,
     webHost: WEB_HOST_NAME,
     appHost: APP_HOST_NAME,
+    scriptUrl: SCRIPT_URL,
     helpCenterUrl: 'https://squeaky.notion.site/Squeaky-Help-Centre-fc049a1822b94b7a8df362811c534d4b',
     helpCenterTrackingCodeUrl: 'https://squeaky.notion.site/Install-your-tracking-code-6ab27212bb5c434196f494ac43349b72',
   },
